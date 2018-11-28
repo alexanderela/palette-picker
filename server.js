993ade542e60ab54766fe6daf5165c0cf249c2bd
Alexander Ela <ela.alexander@gmail.com>
2018-11-27 18:19:45 -0700

    Add post for projects

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const app = express();
 app.locals.projects = [];
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const uuidv4 = require('uuid/v4');
+
+app.use( bodyParser.json() );
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker';
 
 app.use(express.static('public'));
-// app.use( bodyParser.json() );
 
 
 //Project Endpoints
-app.get('/projects', (request, response) => {
+app.get('/api/v1/projects', (request, response) => {
 	const projects = app.locals.projects;
 	return response.json({ projects })
 });
 
-app.get('/projects/:id', (request, response) => {
+app.get('/api/v1/projects/:id', (request, response) => {
 	const { id } = request.params;
 	const project = app.locals.projects.find(project => project.id === id);
 	if (project) {
@@ -26,6 +28,19 @@ app.get('/projects/:id', (request, response) => {
 	}
 });
 
+app.post('/api/v1/projects', (request, response) => {
+	const id = uuidv4();
+	const project = request.body;
+
+	if (!project) {
+		return response.status(422).send({
+			error: 'No project title provided'
+		});
+	} else {
+		app.locals.projects.push({ id, ...project });
+		return response.status(201).json({ id, project });
+	}
+})
 
 
 //Palette Endpoints
