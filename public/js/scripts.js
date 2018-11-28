3b1440e6080de8df016ffecfda4e6c9761971770
Alexander Ela <ela.alexander@gmail.com>
2018-11-28 15:39:37 -0700

    Add randomized hexcode background colors to main swatches

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,17 +1,26 @@
+$(window).on('load', generatePalette)
 $('.lock-btn').on('click', toggleLock)
+$('.new-palette-btn').on('click', generatePalette)
 
-const getRandomColor = () => {
-	const characters = '0123456789abcdef'
-	let color = '#';
-	for(let i = 0; i < 6; i++) {
-		color += characters[Math.floor(Math.random() * 16)]
-	}
-	return color;
+function generatePalette() {
+	$('.color-1').css('background-color', getRandomColor());
+	$('.color-2').css('background-color', getRandomColor());
+	$('.color-3').css('background-color', getRandomColor());
+	$('.color-4').css('background-color', getRandomColor());
+	$('.color-5').css('background-color', getRandomColor());
 }
 
+function getRandomColor() {
+	const characters = '0123456789abcdef';
+	let color = '#';
+	for(let i = 0; i < 6; i++) {
+		color += characters[Math.floor(Math.random() * 16)];
+	};
+	return color;
+};
 
 function toggleLock(e) {
 	const lockButton = $(e.target);
-	console.log(lockButton)
+	console.log(lockButton);
 	lockButton.toggleClass('locked');
-}
+};
