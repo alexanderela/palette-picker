b4eda7886ce2b4287e67aa2be81e4ef6a2845cf0
Alexander Ela <ela.alexander@gmail.com>
2018-11-27 17:31:39 -0700

    Add project GET endpoint

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
 const express = require('express');
 const app = express();
+app.locals.projects = [];
+// const bodyParser = require('body-parser');
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker';
-app.locals.project = {};
-app.locals.palettes = {};
 
-function postProject(id, data) {
-	app.locals.project[id] = {
-		name: 'new Project',
-		palettes: ARRAYOFIDS
-	} 
-}
-
-app.get('/api/projects/:id', (request, response) => {
-	//do your getting
-})
+app.use(express.static('public'));
+// app.use( bodyParser.json() );
 
 
+//Project Endpoints
+app.get('/projects', (request, response) => {
+	const projects = app.locals.projects;
+	return response.json({ projects })
+});
+
+app.get('/projects/:id', (request, response) => {
+	const { id } = request.params;
+	const project = app.locals.projects.find(project => project.id === id);
+	if (project) {
+		return response.status(200).json(project);
+	} else {
+		return response.sendStatus(404);
+	}
+});
+
+
+
+//Palette Endpoints
 
 app.listen(app.get('port'), () => {
 	console.log(`${app.locals.title} is running on ${app.get('port')}`)
 })
+
