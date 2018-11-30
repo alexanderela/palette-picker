98afe62d19de1e301378e9ad7b8c9136ce35d31c
Alexander Ela <ela.alexander@gmail.com>
2018-11-29 18:13:18 -0700

    Add knex get request for palettes for specific project

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.post('/api/v1/projects', (request, response) => {
 // })
 
 //Palette Endpoints
-app.get('/api/v1/palettes', (request, response) => {
+app.get('/api/v1/projects/:project_id/palettes', (request, response) => {
 	database('palettes').select()
 		.then((palettes) => {
 			response.status(200).json(palettes);
@@ -121,6 +121,13 @@ app.post('/api/v1/palettes', (request, response) => {
 				.send({ error: `Expected format: { name: <String>, color1: <String>, color2: <String>, color3: <String>, color4: <String>, color5: <String>, project_id: <Number> You're missing a '${requiredParameter}' property.}` })
 		}
 	}
+	database('palettes').insert(palette, 'id')
+		.then(palette => {
+			response.status(201).json({ id: palette[0] })
+		})
+		.catch(error => {
+			response.status(500).json({ error });
+		});
 })
 
 // app.post('/api/v1/palettes', (request, response) => {
