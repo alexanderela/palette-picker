6f61969fec4e154ca8c9fbbb9b3ab2a4d013ed87
Alexander Ela <ela.alexander@gmail.com>
2019-04-16 23:55:50 -0700

    Add test files

---
+++ server.test.js
@@ -0,0 +1,20 @@
+
+describe('server.js', () => {
+	let server;
+
+	beforeEach(() => {
+		server = require('./server.js');
+	})
+
+	afterEach(() => {
+		server.close()
+	})
+
+	describe('GET /projects', () => {
+		it('should respond to /', (done) => {
+			const res = await request(app).get('/api/v1/projects')
+			const result = res.body
+			expect(result.length).toEqual(0)
+		})
+	})
+})
