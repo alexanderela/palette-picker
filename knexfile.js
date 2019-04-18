76197236a5187f3b1a648da3011eaf78975d3f62
Alexander Ela <ela.alexander@gmail.com>
2019-04-17 23:56:11 -0700

    Add test file architecture

--- knexfile.js
+++ knexfile.js
@@ -13,6 +13,17 @@ module.exports = {
     },
     useNullAsDefault: true
   },
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/palettepicker_test',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      './db/seeds/test'
+    },
+    useNullAsDefault: true,
+  },
 	production: {
 		client: 'pg',
 		connection: process.env.DATABASE_URL + `?ssl=true`,
