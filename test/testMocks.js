1af001be9ac4ee4f98232543f172572faaffdb0e
Alexander Ela <ela.alexander@gmail.com>
2019-04-24 21:33:32 -0600

    Create testMockPalettes

--- test/testMocks.js
+++ test/testMocks.js
@@ -5,3 +5,23 @@ const testMockProjects = [
 	{ name: 'Test Project 4' }
 ]
 
+const testMockPalettes = [
+	{ 
+		name: 'Test Palette 1',
+		project_id: 1,
+		color1: '#938be9',
+		color2: '#d045ae',
+		color3: '#b508e8',
+		color4: '#2f770f',
+		color5: '#68e9cd',
+	},
+	{ 
+		name: 'Test Palette 2',
+		project_id: 1,
+		color1: '#ffffff',
+		color2: '#000000',
+		color3: '#800000',
+		color4: '#ff0000',
+		color5: '#ffa500',
+	},
+]
