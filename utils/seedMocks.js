91cdf7e621ffddb39f575a0051056495c49e95a6
Alexander Ela <ela.alexander@gmail.com>
2019-04-25 22:27:11 -0600

    Add mockProjects and mockPalettes to seedMocks.js

---
+++ utils/seedMocks.js
@@ -0,0 +1,29 @@
+const mockProjects = [
+	{ name: 'Project 1' },
+	{ name: 'Project 2' },
+	{ name: 'Project 3' },
+	{ name: 'Project 4' }
+]
+
+const mockPalettes = [
+	{ 
+		name: 'Test Palette 1',
+		project_id: 1,
+		color1: '#938be9',
+		color2: '#d045ae',
+		color3: '#b508e8',
+		color4: '#2f770f',
+		color5: '#68e9cd',
+	},
+	{ 
+		name: 'Test Palette 2',
+		project_id: 1,
+		color1: '#ffffff',
+		color2: '#000000',
+		color3: '#800000',
+		color4: '#ff0000',
+		color5: '#ffa500',
+	},
+]
+
+module.exports = { mockProjects, mockPalettes }
