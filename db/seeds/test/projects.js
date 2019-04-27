da2e4818ad220e711f7cb11a997bc0ffc331e299
Alexander Ela <ela.alexander@gmail.com>
2019-04-26 19:25:18 -0600

    Recreate exports.seed for test seed

--- db/seeds/test/projects.js
+++ db/seeds/test/projects.js
@@ -6,45 +6,56 @@ const createProjects = (knex, project) => {
 	}, 'id')
 }
 
-const createPalettes = (knex, palette) => {
-	return knex('palettes').insert(palette)
-}
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('palettes').del()
   	.then(() => knex('projects').del())
     .then(() => {
       // Inserts seed entries
-      return Promise.all([
-      	knex('projects').insert({
-      		name: 'test seed project'
-      	}, 'id')
-      	.then(project => {
-      		return knex('palettes').insert([
-						{ 
-							name: 'Test Palette 1',
-							color1: '#938be9',
-							color2: '#d045ae',
-							color3: '#b508e8',
-							color4: '#2f770f',
-							color5: '#68e9cd',
-							project_id: project[0],
-						},
-						{ 
-							name: 'Test Palette 2',
-							color1: '#ffffff',
-							color2: '#000000',
-							color3: '#800000',
-							color4: '#ff0000',
-							color5: '#ffa500',
-							project_id: project[0],
-						}
-      		])
-      	})
-      	.then(() => console.log('Seeding complete!'))
-      	.catch(error => console.log(`Error seeding data: ${error}`))
-    	])
+      let projectPromises = mockProjects.map(project => {
+      	return createProjects(knex, project)
+      })
+      return Promise.all(projectPromises)
     })
-    .catch(error => console.log(`Error seeding data: ${error}`))
+    .then(() => console.log('Successfully seeded db!'))
+    .catch(error => console.log(`Error seeding db: ${error.message}`))
 };
+
+// exports.seed = function(knex, Promise) {
+//   // Deletes ALL existing entries
+//   return knex('palettes').del()
+//   	.then(() => knex('projects').del())
+//     .then(() => {
+//       // Inserts seed entries
+//       return Promise.all([
+//       	knex('projects').insert({
+//       		name: 'test seed project'
+//       	}, 'id')
+//       	.then(project => {
+//       		return knex('palettes').insert([
+// 						{ 
+// 							name: 'Test Palette 1',
+// 							color1: '#938be9',
+// 							color2: '#d045ae',
+// 							color3: '#b508e8',
+// 							color4: '#2f770f',
+// 							color5: '#68e9cd',
+// 							project_id: project[0],
+// 						},
+// 						{ 
+// 							name: 'Test Palette 2',
+// 							color1: '#ffffff',
+// 							color2: '#000000',
+// 							color3: '#800000',
+// 							color4: '#ff0000',
+// 							color5: '#ffa500',
+// 							project_id: project[0],
+// 						}
+//       		])
+//       	})
+//       	.then(() => console.log('Seeding complete!'))
+//       	.catch(error => console.log(`Error seeding data: ${error}`))
+//     	])
+//     })
+//     .catch(error => console.log(`Error seeding data: ${error}`))
+// };
